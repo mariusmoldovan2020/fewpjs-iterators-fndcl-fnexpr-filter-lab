a59f27d7b43529297b78903cb267c1e0ca82d554
mariusmoldovan <biris_21@yahoo.com>
2020-11-03 17:29:54 -0600

    finish filter lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(source, string) {
+    return source.filter( match =>
+      match.toLowerCase() === string.toLowerCase()
+    )
+  }
+
+
+  function fuzzyMatch(source, string) {
+    return source.filter( match =>
+      match.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+  }
+
+  function matchName(drivers, name) {
+    return drivers.filter( record => record.name === name)
+  }
